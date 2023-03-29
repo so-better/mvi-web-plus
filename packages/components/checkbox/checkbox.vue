96095e3f5db86151c78c26656a6762f02a129fb6
lovelin0523 <18356201582@163.com>
2023-03-29 11:15:12 +0800

    更新优化

--- packages/components/checkbox/checkbox.vue
+++ packages/components/checkbox/checkbox.vue
@@ -1,173 +1,173 @@
 <template>
-    <label class="mvi-checkbox" :disabled="disabled || null">
-        <span v-if="labelPlacement=='left' && label" :disabled="disabled || null" class="mvi-checkbox-label" :data-placement="labelPlacement" v-text="label" :style="labelStyle"></span>
-        <input @change="change" :value="value" :disabled="disabled || null" :checked="check" type="checkbox" :name="name" />
-        <span :disabled="disabled || null" :class="['mvi-checkbox-item',check?'mvi-checkbox-item-check':'']" :style="checkboxStyle">
-            <Icon :disabled="disabled || null" :color="iconColor" :size="iconSize" :type="iconType" :class="['mvi-checkbox-icon',check?'mvi-checkbox-icon-check':'']" />
-        </span>
-        <span v-if="labelPlacement=='right' && label" :disabled="disabled || null" class="mvi-checkbox-label" :data-placement="labelPlacement" v-text="label" :style="labelStyle"></span>
-    </label>
+	<label class="mvi-checkbox" :disabled="disabled || null">
+		<span v-if="labelPlacement == 'left' && label" :disabled="disabled || null" class="mvi-checkbox-label" :data-placement="labelPlacement" v-text="label" :style="labelStyle"></span>
+		<input @change="change" :value="value" :disabled="disabled || null" :checked="check" type="checkbox" :name="name" />
+		<span :disabled="disabled || null" :class="['mvi-checkbox-item', check ? 'mvi-checkbox-item-check' : '']" :style="checkboxStyle">
+			<Icon :disabled="disabled || null" :color="iconColor" :size="iconSize" :type="iconType" :class="['mvi-checkbox-icon', check ? 'mvi-checkbox-icon-check' : '']" />
+		</span>
+		<span v-if="labelPlacement == 'right' && label" :disabled="disabled || null" class="mvi-checkbox-label" :data-placement="labelPlacement" v-text="label" :style="labelStyle"></span>
+	</label>
 </template>
 
 <script>
 import { Dap } from '../dap'
 import { Icon } from '../icon'
 export default {
-    name: 'm-checkbox',
-    emits: ['update:modelValue', 'change'],
-    props: {
-        //是否禁用
-        disabled: {
-            type: Boolean,
-            default: false
-        },
-        //是否选中
-        modelValue: {
-            type: [Boolean, Array],
-            default: false
-        },
-        //label文字
-        label: {
-            type: String,
-            default: null
-        },
-        //文字颜色
-        labelColor: {
-            type: String,
-            default: null
-        },
-        //文字大小
-        labelSize: {
-            type: String,
-            default: null
-        },
-        //值
-        value: {
-            type: [Object, Number, String, Array],
-            default: ''
-        },
-        //填充颜色
-        fillColor: {
-            type: String,
-            default: null
-        },
-        //文字位置
-        labelPlacement: {
-            type: String,
-            default: 'right',
-            validator(value) {
-                return ['left', 'right'].includes(value)
-            }
-        },
-        //是否圆形
-        round: {
-            type: Boolean,
-            default: false
-        },
-        //图标
-        icon: {
-            type: [String, Object],
-            default: 'success'
-        },
-        //原生name属性
-        name: {
-            type: String,
-            default: null
-        }
-    },
-    computed: {
-        iconColor() {
-            if (this.disabled || !this.check) {
-                return null
-            }
-            let color = null
-            if (Dap.common.isObject(this.icon)) {
-                if (typeof this.icon.color == 'string') {
-                    color = this.icon.color
-                }
-            }
-            return color
-        },
-        iconSize() {
-            let size = null
-            if (Dap.common.isObject(this.icon)) {
-                if (typeof this.icon.size == 'string') {
-                    size = this.icon.size
-                }
-            }
-            return size
-        },
-        iconType() {
-            let type = 'success'
-            if (Dap.common.isObject(this.icon)) {
-                if (typeof this.icon.type == 'string') {
-                    type = this.icon.type
-                }
-            } else if (typeof this.icon == 'string') {
-                type = this.icon
-            }
-            return type
-        },
-        checkboxStyle() {
-            let style = {}
-            if (!this.disabled && this.check && this.fillColor) {
-                style.backgroundColor = this.fillColor
-                style.borderColor = this.fillColor
-            }
-            if (this.round) {
-                style.borderRadius = '50%'
-            }
-            return style
-        },
-        labelStyle() {
-            let style = {}
-            if (!this.disabled && this.labelColor) {
-                style.color = this.labelColor
-            }
-            if (this.labelSize) {
-                style.fontSize = this.labelSize
-            }
-            return style
-        },
-        check() {
-            //modelValue为boolean
-            if (typeof this.modelValue == 'boolean') {
-                return this.modelValue
-            } else if (Array.isArray(this.modelValue)) {
-                //数组中是否已包含此复选框的值
-                return this.modelValue.some(item => {
-                    return Dap.common.equal(item, this.value)
-                })
-            } else {
-                return false
-            }
-        }
-    },
-    components: {
-        Icon
-    },
-    methods: {
-        change(event) {
-            if (Array.isArray(this.modelValue)) {
-                let arr = [...this.modelValue]
-                //勾选且不包含
-                if (event.target.checked && !this.check) {
-                    arr.push(this.value)
-                }
-                //取消且包含
-                else if (this.check) {
-                    arr = arr.filter(item => {
-                        return !Dap.common.equal(item, this.value)
-                    })
-                }
-                this.$emit('update:modelValue', arr)
-                this.$emit('change', arr)
-            } else if (typeof this.modelValue == 'boolean') {
-                this.$emit('update:modelValue', event.target.checked)
-                this.$emit('change', event.target.checked)
-            }
-        }
-    }
+	name: 'm-checkbox',
+	emits: ['update:modelValue', 'change'],
+	props: {
+		//是否禁用
+		disabled: {
+			type: Boolean,
+			default: false
+		},
+		//是否选中
+		modelValue: {
+			type: [Boolean, Array],
+			default: false
+		},
+		//label文字
+		label: {
+			type: String,
+			default: null
+		},
+		//文字颜色
+		labelColor: {
+			type: String,
+			default: null
+		},
+		//文字大小
+		labelSize: {
+			type: String,
+			default: null
+		},
+		//值
+		value: {
+			type: [Object, Number, String, Array],
+			default: ''
+		},
+		//填充颜色
+		fillColor: {
+			type: String,
+			default: null
+		},
+		//文字位置
+		labelPlacement: {
+			type: String,
+			default: 'right',
+			validator(value) {
+				return ['left', 'right'].includes(value)
+			}
+		},
+		//是否圆形
+		round: {
+			type: Boolean,
+			default: false
+		},
+		//图标
+		icon: {
+			type: [String, Object],
+			default: 'success'
+		},
+		//原生name属性
+		name: {
+			type: String,
+			default: null
+		}
+	},
+	computed: {
+		iconColor() {
+			if (this.disabled || !this.check) {
+				return null
+			}
+			let color = null
+			if (Dap.common.isObject(this.icon)) {
+				if (typeof this.icon.color == 'string') {
+					color = this.icon.color
+				}
+			}
+			return color
+		},
+		iconSize() {
+			let size = null
+			if (Dap.common.isObject(this.icon)) {
+				if (typeof this.icon.size == 'string') {
+					size = this.icon.size
+				}
+			}
+			return size
+		},
+		iconType() {
+			let type = 'success'
+			if (Dap.common.isObject(this.icon)) {
+				if (typeof this.icon.type == 'string') {
+					type = this.icon.type
+				}
+			} else if (typeof this.icon == 'string') {
+				type = this.icon
+			}
+			return type
+		},
+		checkboxStyle() {
+			let style = {}
+			if (!this.disabled && this.check && this.fillColor) {
+				style.backgroundColor = this.fillColor
+				style.borderColor = this.fillColor
+			}
+			if (this.round) {
+				style.borderRadius = '50%'
+			}
+			return style
+		},
+		labelStyle() {
+			let style = {}
+			if (!this.disabled && this.labelColor) {
+				style.color = this.labelColor
+			}
+			if (this.labelSize) {
+				style.fontSize = this.labelSize
+			}
+			return style
+		},
+		check() {
+			//modelValue为boolean
+			if (typeof this.modelValue == 'boolean') {
+				return this.modelValue
+			} else if (Array.isArray(this.modelValue)) {
+				//数组中是否已包含此复选框的值
+				return this.modelValue.some(item => {
+					return Dap.common.equal(item, this.value)
+				})
+			} else {
+				return false
+			}
+		}
+	},
+	components: {
+		Icon
+	},
+	methods: {
+		change(event) {
+			if (Array.isArray(this.modelValue)) {
+				let arr = [...this.modelValue]
+				//勾选且不包含
+				if (event.target.checked && !this.check) {
+					arr.push(this.value)
+				}
+				//取消且包含
+				else if (this.check) {
+					arr = arr.filter(item => {
+						return !Dap.common.equal(item, this.value)
+					})
+				}
+				this.$emit('update:modelValue', arr)
+				this.$emit('change', arr)
+			} else if (typeof this.modelValue == 'boolean') {
+				this.$emit('update:modelValue', event.target.checked)
+				this.$emit('change', event.target.checked)
+			}
+		}
+	}
 }
 </script>
 
@@ -175,83 +175,83 @@ export default {
 @import '../../css/mvi-basic.less';
 
 .mvi-checkbox {
-    display: inline-flex;
-    margin: 0;
-    padding: 0;
-    position: relative;
-    vertical-align: middle;
-    justify-content: flex-start;
-    align-items: center;
-    cursor: pointer;
-    user-select: none;
-    -webkit-user-select: none;
+	display: inline-flex;
+	margin: 0;
+	padding: 0;
+	position: relative;
+	vertical-align: middle;
+	justify-content: flex-start;
+	align-items: center;
+	cursor: pointer;
+	user-select: none;
+	-webkit-user-select: none;
 
-    & > input[type='checkbox'] {
-        width: 0;
-        height: 0;
-        opacity: 0;
-        border: none;
-        display: none;
-    }
+	& > input[type='checkbox'] {
+		width: 0;
+		height: 0;
+		opacity: 0;
+		border: none;
+		display: none;
+	}
 }
 
 .mvi-checkbox-item {
-    display: inline-flex;
-    display: -webkit-inline-flex;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-    margin: 0;
-    padding: @mp-xs / 2;
-    border: 1px solid @border-color;
-    border-radius: @radius-default;
-    background-color: #fff;
-    transition: background-color 100ms, border-color 100ms;
+	display: inline-flex;
+	display: -webkit-inline-flex;
+	justify-content: center;
+	align-items: center;
+	position: relative;
+	margin: 0;
+	padding: @mp-xs / 2;
+	border: 1px solid @border-color;
+	border-radius: @radius-default;
+	background-color: #fff;
+	transition: background-color 100ms, border-color 100ms;
 
-    &.mvi-checkbox-item-check {
-        background-color: @info-normal;
-        border-color: @info-normal;
-    }
+	&.mvi-checkbox-item-check {
+		background-color: @info-normal;
+		border-color: @info-normal;
+	}
 
-    &[disabled],
-    &.mvi-checkbox-item-check[disabled] {
-        background-color: @bg-color-dark;
-        border-color: @border-color;
-    }
+	&[disabled],
+	&.mvi-checkbox-item-check[disabled] {
+		background-color: @bg-color-dark;
+		border-color: @border-color;
+	}
 }
 
 .mvi-checkbox-icon {
-    font-size: @font-size-default;
-    margin: 0;
-    padding: 0;
-    line-height: 1;
-    color: transparent;
-    transition: color 300ms;
+	font-size: @font-size-small;
+	margin: 0;
+	padding: 0;
+	line-height: 1;
+	color: transparent;
+	transition: color 300ms;
 
-    &.mvi-checkbox-icon-check {
-        color: #fff;
+	&.mvi-checkbox-icon-check {
+		color: #fff;
 
-        &[disabled] {
-            color: @font-color-mute;
-        }
-    }
+		&[disabled] {
+			color: @font-color-mute;
+		}
+	}
 }
 .mvi-checkbox-label {
-    vertical-align: middle;
-    font-size: @font-size-default;
-    color: @font-color-default;
-    user-select: none;
+	vertical-align: middle;
+	font-size: @font-size-default;
+	color: @font-color-default;
+	user-select: none;
 
-    &[data-placement='left'] {
-        margin-right: @mp-sm;
-    }
+	&[data-placement='left'] {
+		margin-right: @mp-xs;
+	}
 
-    &[data-placement='right'] {
-        margin-left: @mp-sm;
-    }
+	&[data-placement='right'] {
+		margin-left: @mp-xs;
+	}
 
-    &[disabled] {
-        color: @font-color-mute;
-    }
+	&[disabled] {
+		color: @font-color-mute;
+	}
 }
 </style>
--- packages/components/radio/radio.vue
+++ packages/components/radio/radio.vue
@@ -1,160 +1,160 @@
 <template>
-    <label :disabled="disabled || null" class="mvi-radio">
-        <span :disabled="disabled || null" v-if="label && labelPlacement=='left'" class="mvi-radio-label" :data-placement="labelPlacement" v-text="label" :style="labelStyle"></span>
-        <input @change="change" :value="value" :disabled="disabled || null" :checked="check" type="radio" :name="name" />
-        <span :disabled="disabled || null" :class="['mvi-radio-item',check?'mvi-radio-item-check':'']" :style="radioStyle">
-            <Icon :disabled="disabled || null" :color="iconColor" :size="iconSize" :type="iconType" :class="['mvi-radio-icon',check?'mvi-radio-icon-check':'']" />
-        </span>
-        <span :disabled="disabled || null" v-if="label && labelPlacement == 'right'" class="mvi-radio-label" :data-placement="labelPlacement" v-text="label" :style="labelStyle"></span>
-    </label>
+	<label :disabled="disabled || null" class="mvi-radio">
+		<span :disabled="disabled || null" v-if="label && labelPlacement == 'left'" class="mvi-radio-label" :data-placement="labelPlacement" v-text="label" :style="labelStyle"></span>
+		<input @change="change" :value="value" :disabled="disabled || null" :checked="check" type="radio" :name="name" />
+		<span :disabled="disabled || null" :class="['mvi-radio-item', check ? 'mvi-radio-item-check' : '']" :style="radioStyle">
+			<Icon :disabled="disabled || null" :color="iconColor" :size="iconSize" :type="iconType" :class="['mvi-radio-icon', check ? 'mvi-radio-icon-check' : '']" />
+		</span>
+		<span :disabled="disabled || null" v-if="label && labelPlacement == 'right'" class="mvi-radio-label" :data-placement="labelPlacement" v-text="label" :style="labelStyle"></span>
+	</label>
 </template>
 
 <script>
 import { Dap } from '../dap'
 import { Icon } from '../icon'
 export default {
-    name: 'm-radio',
-    emits: ['update:modelValue', 'change'],
-    props: {
-        //单选框的值
-        value: {
-            type: [Object, Number, String],
-            default: ''
-        },
-        //是否选中
-        modelValue: {
-            type: [Boolean, String, Number, Object],
-            default: false
-        },
-        //是否禁用
-        disabled: {
-            type: Boolean,
-            default: false
-        },
-        //label文字
-        label: {
-            type: String,
-            default: null
-        },
-        //文字位置
-        labelPlacement: {
-            type: String,
-            default: 'right',
-            validator(value) {
-                return ['left', 'right'].includes(value)
-            }
-        },
-        //文字颜色
-        labelColor: {
-            type: String,
-            default: null
-        },
-        //文字尺寸
-        labelSize: {
-            type: String,
-            default: null
-        },
-        //填充颜色
-        fillColor: {
-            type: String,
-            default: null
-        },
-        //图标
-        icon: {
-            type: [String, Object],
-            default: 'success'
-        },
-        //是否圆形
-        round: {
-            type: Boolean,
-            default: true
-        },
-        //原生name
-        name: {
-            type: String,
-            default: null
-        }
-    },
-    computed: {
-        labelStyle() {
-            let style = {}
-            if (!this.disabled && this.labelColor) {
-                style.color = this.labelColor
-            }
-            if (this.labelSize) {
-                style.fontSize = this.labelSize
-            }
-            return style
-        },
-        iconColor() {
-            if (this.disabled || !this.check) {
-                return null
-            }
-            let color = null
-            if (Dap.common.isObject(this.icon)) {
-                if (typeof this.icon.color == 'string') {
-                    color = this.icon.color
-                }
-            }
-            return color
-        },
-        iconSize() {
-            let size = null
-            if (Dap.common.isObject(this.icon)) {
-                if (typeof this.icon.size == 'string') {
-                    size = this.icon.size
-                }
-            }
-            return size
-        },
-        iconType() {
-            let type = 'success'
-            if (Dap.common.isObject(this.icon)) {
-                if (typeof this.icon.type == 'string') {
-                    type = this.icon.type
-                }
-            } else if (typeof this.icon == 'string') {
-                type = this.icon
-            }
-            return type
-        },
-        radioStyle() {
-            let style = {}
-            if (!this.disabled && this.check && this.fillColor) {
-                style.backgroundColor = this.fillColor
-                style.borderColor = this.fillColor
-            }
-            if (this.round) {
-                style.borderRadius = '50%'
-            }
-            return style
-        },
-        check() {
-            //modelValue为boolean
-            if (typeof this.modelValue == 'boolean') {
-                return this.modelValue
-            } else {
-                return Dap.common.equal(this.modelValue, this.value)
-            }
-        }
-    },
-    components: {
-        Icon
-    },
-    methods: {
-        change(event) {
-            if (typeof this.modelValue == 'boolean') {
-                this.$emit('update:modelValue', event.target.checked)
-                this.$emit('change', event.target.checked)
-            } else {
-                //勾选
-                if (event.target.checked) {
-                    this.$emit('update:modelValue', this.value)
-                    this.$emit('change', this.value)
-                }
-            }
-        }
-    }
+	name: 'm-radio',
+	emits: ['update:modelValue', 'change'],
+	props: {
+		//单选框的值
+		value: {
+			type: [Object, Number, String],
+			default: ''
+		},
+		//是否选中
+		modelValue: {
+			type: [Boolean, String, Number, Object],
+			default: false
+		},
+		//是否禁用
+		disabled: {
+			type: Boolean,
+			default: false
+		},
+		//label文字
+		label: {
+			type: String,
+			default: null
+		},
+		//文字位置
+		labelPlacement: {
+			type: String,
+			default: 'right',
+			validator(value) {
+				return ['left', 'right'].includes(value)
+			}
+		},
+		//文字颜色
+		labelColor: {
+			type: String,
+			default: null
+		},
+		//文字尺寸
+		labelSize: {
+			type: String,
+			default: null
+		},
+		//填充颜色
+		fillColor: {
+			type: String,
+			default: null
+		},
+		//图标
+		icon: {
+			type: [String, Object],
+			default: 'success'
+		},
+		//是否圆形
+		round: {
+			type: Boolean,
+			default: true
+		},
+		//原生name
+		name: {
+			type: String,
+			default: null
+		}
+	},
+	computed: {
+		labelStyle() {
+			let style = {}
+			if (!this.disabled && this.labelColor) {
+				style.color = this.labelColor
+			}
+			if (this.labelSize) {
+				style.fontSize = this.labelSize
+			}
+			return style
+		},
+		iconColor() {
+			if (this.disabled || !this.check) {
+				return null
+			}
+			let color = null
+			if (Dap.common.isObject(this.icon)) {
+				if (typeof this.icon.color == 'string') {
+					color = this.icon.color
+				}
+			}
+			return color
+		},
+		iconSize() {
+			let size = null
+			if (Dap.common.isObject(this.icon)) {
+				if (typeof this.icon.size == 'string') {
+					size = this.icon.size
+				}
+			}
+			return size
+		},
+		iconType() {
+			let type = 'success'
+			if (Dap.common.isObject(this.icon)) {
+				if (typeof this.icon.type == 'string') {
+					type = this.icon.type
+				}
+			} else if (typeof this.icon == 'string') {
+				type = this.icon
+			}
+			return type
+		},
+		radioStyle() {
+			let style = {}
+			if (!this.disabled && this.check && this.fillColor) {
+				style.backgroundColor = this.fillColor
+				style.borderColor = this.fillColor
+			}
+			if (this.round) {
+				style.borderRadius = '50%'
+			}
+			return style
+		},
+		check() {
+			//modelValue为boolean
+			if (typeof this.modelValue == 'boolean') {
+				return this.modelValue
+			} else {
+				return Dap.common.equal(this.modelValue, this.value)
+			}
+		}
+	},
+	components: {
+		Icon
+	},
+	methods: {
+		change(event) {
+			if (typeof this.modelValue == 'boolean') {
+				this.$emit('update:modelValue', event.target.checked)
+				this.$emit('change', event.target.checked)
+			} else {
+				//勾选
+				if (event.target.checked) {
+					this.$emit('update:modelValue', this.value)
+					this.$emit('change', this.value)
+				}
+			}
+		}
+	}
 }
 </script>
 
@@ -162,81 +162,81 @@ export default {
 @import '../../css/mvi-basic.less';
 
 .mvi-radio {
-    display: inline-flex;
-    margin: 0;
-    padding: 0;
-    vertical-align: middle;
-    justify-content: flex-start;
-    align-items: center;
-    position: relative;
-    cursor: pointer;
+	display: inline-flex;
+	margin: 0;
+	padding: 0;
+	vertical-align: middle;
+	justify-content: flex-start;
+	align-items: center;
+	position: relative;
+	cursor: pointer;
 
-    & > input[type='radio'] {
-        width: 0;
-        height: 0;
-        opacity: 0;
-        border: none;
-        display: none;
-    }
+	& > input[type='radio'] {
+		width: 0;
+		height: 0;
+		opacity: 0;
+		border: none;
+		display: none;
+	}
 }
 
 .mvi-radio-item {
-    display: inline-flex;
-    display: -webkit-inline-flex;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-    margin: 0;
-    padding: @mp-xs / 2;
-    border: 1px solid @border-color;
-    background-color: #fff;
-    transition: background-color 100ms, border-color 100ms;
+	display: inline-flex;
+	display: -webkit-inline-flex;
+	justify-content: center;
+	align-items: center;
+	position: relative;
+	margin: 0;
+	padding: @mp-xs / 2;
+	border: 1px solid @border-color;
+	background-color: #fff;
+	transition: background-color 100ms, border-color 100ms;
 
-    &.mvi-radio-item-check {
-        background-color: @info-normal;
-        border-color: @info-normal;
-    }
+	&.mvi-radio-item-check {
+		background-color: @info-normal;
+		border-color: @info-normal;
+	}
 
-    &[disabled],
-    &.mvi-radio-item-check[disabled] {
-        background-color: @bg-color-dark;
-        border-color: @border-color;
-    }
+	&[disabled],
+	&.mvi-radio-item-check[disabled] {
+		background-color: @bg-color-dark;
+		border-color: @border-color;
+	}
 }
 
 .mvi-radio-icon {
-    font-size: @font-size-default;
-    margin: 0;
-    padding: 0;
-    line-height: 1;
-    color: transparent;
-    transition: color 300ms;
+	font-size: @font-size-small;
+	margin: 0;
+	padding: 0;
+	line-height: 1;
+	color: transparent;
+	transition: color 300ms;
 
-    &.mvi-radio-icon-check {
-        color: #fff;
+	&.mvi-radio-icon-check {
+		color: #fff;
 
-        &[disabled] {
-            color: @font-color-mute;
-        }
-    }
+		&[disabled] {
+			color: @font-color-mute;
+		}
+	}
 }
 
 .mvi-radio-label {
-    vertical-align: middle;
-    font-size: @font-size-default;
-    color: @font-color-default;
-    user-select: none;
+	vertical-align: middle;
+	font-size: @font-size-default;
+	color: @font-color-default;
+	user-select: none;
 
-    &[data-placement='left'] {
-        margin-right: @mp-xs;
-    }
+	&[data-placement='left'] {
+		margin-right: @mp-xs;
+	}
 
-    &[data-placement='right'] {
-        margin-left: @mp-xs;
-    }
+	&[data-placement='right'] {
+		margin-left: @mp-xs;
+	}
 
-    &[disabled] {
-        color: @font-color-mute;
-    }
+	&[disabled] {
+		color: @font-color-mute;
+	}
 }
 </style>
