a98ff05d73dfd6bf7a43ffe6cd80977bdc309398
lovelin0523 <18356201582@163.com>
2023-04-17 17:28:41 +0800

    更新

--- example/App.vue
+++ example/App.vue
@@ -3,7 +3,7 @@
 		<div>{{ value }}</div>
 		<m-button @click="disabled = !disabled">按钮</m-button>
 		<m-editor-menus border ref="editorMenus"></m-editor-menus>
-		<m-editor @format-elements="formatElements" :disabled="disabled" border placeholder="Please Enter Text..." v-model="value" ref="editor"></m-editor>
+		<m-editor active-color="#f30" border placeholder="Please Enter Text..." v-model="value" ref="editor"></m-editor>
 	</div>
 </template>
 <script>
@@ -12,7 +12,7 @@ export default {
 		return {
 			show: true,
 			disabled: false,
-			value: '<table mvi-editor-element="32" cellpadding="0" cellspacing="0"><colgroup><col width="auto"><col width="auto"><col width="auto"><col width="auto"><col width="auto"><col width="auto"><col width="auto"></colgroup><tbody mvi-editor-element="32"><tr mvi-editor-element="32"><td mvi-editor-element="32"><br><span contenteditable="false" class="mvi-editor-colbtn"></span></td><td mvi-editor-element="32"><br><span contenteditable="false" class="mvi-editor-colbtn"></span></td><td mvi-editor-element="32"><br><span contenteditable="false" class="mvi-editor-colbtn"></span></td><td mvi-editor-element="32"><br><span contenteditable="false" class="mvi-editor-colbtn"></span></td><td mvi-editor-element="32"><br><span contenteditable="false" class="mvi-editor-colbtn"></span></td><td mvi-editor-element="32"><br><span contenteditable="false" class="mvi-editor-colbtn"></span></td><td mvi-editor-element="32"><br></td></tr><tr mvi-editor-element="32"><td mvi-editor-element="32"><br></td><td mvi-editor-element="32"><br></td><td mvi-editor-element="32"><br></td><td mvi-editor-element="32"><br></td><td mvi-editor-element="32"><br></td><td mvi-editor-element="32"><br></td><td mvi-editor-element="32"><br></td></tr><tr mvi-editor-element="32"><td mvi-editor-element="32"><br></td><td mvi-editor-element="32"><br></td><td mvi-editor-element="32"><br></td><td mvi-editor-element="32"><br></td><td mvi-editor-element="32"><br></td><td mvi-editor-element="32"><br></td><td mvi-editor-element="32"><br></td></tr></tbody></table>'
+			value: ''
 		}
 	},
 	mounted() {
