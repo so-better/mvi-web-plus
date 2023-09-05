98225ae597d41c8b9b89f587d12b49e907d51169
lovelin0523 <18356201582@163.com>
2023-09-05 17:34:08 +0800

    更新

--- example/main.js
+++ example/main.js
@@ -1,8 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import mvi from '../packages'
-import RemFit from 'rem-fit'
-// const remFit = new RemFit(20, true)
-//remFit.init()
 const app = createApp(App)
 app.use(mvi).mount('#app')
