15558c7826d446f67641880a0ae503a6a72f1983
lovelin0523 <18356201582@163.com>
2024-03-14 09:39:37 +0800

    新增路由测试

--- example/router/index.ts
+++ example/router/index.ts
@@ -1,8 +1,31 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import routes from './routes'
+import { RouteRecordRaw, createRouter, createWebHashHistory } from 'vue-router'
+import { components } from '../../packages'
 const router = createRouter({
 	history: createWebHashHistory('/'),
-	routes
+	routes: [
+		{
+			path: '/',
+			name: 'index',
+			meta: {
+				title: 'mvi-web-plus'
+			},
+			component: () => import('../test/Index.vue')
+		}
+	]
 })
+
+//判断是否vue组件
+const isVueComponent = (item: any) => {
+	return typeof item.render == 'function' || typeof item.setup == 'function'
+}
+
+for (let key in components) {
+	const route: RouteRecordRaw = {
+		path: `/${key}`,
+		name: key,
+		component: () => import(/* @vite-ignore */ `../test/${isVueComponent(components[key]) ? 'components' : 'directives'}/${key}.vue`)
+	}
+	router.addRoute(route)
+}
 
 export default router
