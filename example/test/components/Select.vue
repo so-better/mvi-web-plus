5426d0345d4af7d15af71b194bee0cfd7f9d3ae9
lovelin0523 <18356201582@163.com>
2024-03-17 19:39:32 +0800

    更新测试demo

---
+++ example/test/components/Select.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="mvi-p-2">
+		<m-select multiple v-model="val" size="medium" placeholder="请选择" :options="options"></m-select>
+	</div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const val = ref([])
+const options = ref([
+	{
+		label: 'ElementPlus',
+		value: 0
+	},
+	{
+		label: 'Antd UI',
+		value: 1
+	}
+])
+</script>
+<style lang="less"></style>
