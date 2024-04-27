755ee2c7b14e498a4a46269fc8ecff2d14124a2b
lovelin0523 <18356201582@163.com>
2024-04-27 15:31:21 +0800

    更新

--- packages/index.ts
+++ packages/index.ts
@@ -178,3 +178,5 @@ const version = '1.9.14'
 
 //导出
 export { install as default, install, version }
+
+console.log(`%c mvi-web-plus %c v${version} `, 'padding: 2px 1px; border-radius: 3px 0 0 3px; color: #fff; background: #606060; font-weight: bold;', 'padding: 2px 1px; border-radius: 0 3px 3px 0; color: #fff; background: #42c02e; font-weight: bold;')
